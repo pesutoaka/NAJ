d3f73c81a52fc7253d9dc58354afe6b2215a9b9b
Kamil Kuliński <kamilkulinski>
2015-03-13 14:25:28 +0100

    function "newObject"

--- Lesson 3 - Objects/code/newObject/newObject.js
+++ Lesson 3 - Objects/code/newObject/newObject.js
@@ -1,11 +1,28 @@
 (function (global) {
-	if (!global.UAM) {
-		global.UAM = {};
-	}
+    global.UAM = global.UAM || {};
 
-	function newObject() {}
+	function newObject(myObject) {
+        //http://stackoverflow.com/questions/6750880/javascript-how-does-new-work-internally
+        
+        //     var obj, ret, proto;
+
+        // Check if `myObject.prototype` is an object, not a primitive
+        proto = Object(myObject.prototype) === myObject.prototype ? myObject.prototype : Object.prototype;
+
+        // Create an object that inherits from `proto`
+        obj = Object.create(proto);
+        
+        // Apply the function setting `obj` as the `this` value
+        ret = myObject.apply(obj, Array.prototype.slice.call(arguments, 1));
+        
+        if (Object(ret) === ret) { // the result is an object?
+            return ret;
+        }
+        return obj;
+    }
 
 	global.UAM.newObject = newObject;
+    
 }(window));
 
 /*
@@ -14,5 +31,3 @@
 
 	new MyClass(arg1, arg2) -> newObject(MyClass, arg1, arg2)
 */
-
-
